183f8ec048bd59c4798609c98b1529c85bf02f33
Vladis <vladismarkin@gmail.com>
2021-02-15 18:58:49 +0200

    Adding dummy routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const pageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const pageTwo = () => {
+  return <div>PageTwo</div>;
+};
 
 const App = () => {
-  return <div>App!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={pageOne} />
+          <Route path="/pagetwo" exact component={pageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
