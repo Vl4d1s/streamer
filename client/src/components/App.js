1b301b3e82f97e8cfcc819073f03a5415d7e796a
Vladis <vladismarkin@gmail.com>
2021-02-16 20:11:00 +0200

    Adding paths

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-
-const pageOne = () => {
-  return (
-    <div>
-      <Link to="/pagetwo">PageTwo</Link>
-    </div>
-  );
-};
-
-const pageTwo = () => {
-  return (
-    <div>
-      <Link to="/">PageOne</Link>
-    </div>
-  );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          {/* exact == exact={true} */}
-          <Route path="/" exact component={pageOne} />
-          <Route path="/pagetwo" exact component={pageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
