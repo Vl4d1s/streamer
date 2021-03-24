18bd52a1f055201dd8808ebc02e42d9dcda5dec9
Vladis <vladismarkin@gmail.com>
2021-03-25 01:24:25 +0200

    better syntax to the input element

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,24 @@
 import React from 'react';
+// reduxForm is like the connect function
+// that we use from react-redux libery
+// he dosnt really knowing what we are doing
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInpit(formProps) {
+    return <input {...formProps.input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInpit} />
+        <Field name="description" component={this.renderInpit} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
